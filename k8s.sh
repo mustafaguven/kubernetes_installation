af2c2bb99cd992d01cd814ed2b58cd0a67464af0
MUSTAFA GÜVEN <mustafaguven@gmail.com>
2023-08-30 19:23:18 +0300

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -1,6 +1,7 @@
 !#/bin/bash
 
 sudo apt install curl apt-transport-https -y
+sudp apt install gpg -y
 curl -fsSL  https://packages.cloud.google.com/apt/doc/apt-key.gpg|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/k8s.gpg
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
